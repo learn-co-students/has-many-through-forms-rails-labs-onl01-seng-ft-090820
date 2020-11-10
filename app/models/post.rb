0461fa0630033dd2752d14f9f0a139102b0fcd08
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2020-11-10 16:33:03 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,26 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories 
+  accepts_nested_attributes_for :comments, reject_if: :second_comment
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      # binding.pry
+      if category_attribute["name"] != ""
+        category = Category.find_or_create_by(category_attribute)
+        self.post_categories.build(category: category)
+      end
+    end
+  end
+
+  def second_comment(category_attributes)
+    binding.pry
+    # post.users.include?(User.find_by(categories_attributes["user"]))
+  end
+
+  def unique_users
+    unique_users = self.users.uniq
+  end
 
 end
