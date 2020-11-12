7815ad3e76c2d53abd5c69411a6bf15a837cb21b
doosemavis <moosedavis2011@gmail.com>
2020-11-12 14:46:56 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,15 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  
+
+  def categories_attributes=(attribute_hash)
+    attribute_hash.values.each do |ah|
+      if !ah[:name].blank?
+        self.categories.build(ah)
+      end 
+    end 
+  end
 
 
 end
